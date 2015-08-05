54aa40b07cd60107620ba287a36b55100b6f0007
张德荣 <zdrjson@sina.com>
2015-08-05 16:42:15 +0800

    加入git分支指令

--- TestGit/ViewController.m
+++ TestGit/ViewController.m
@@ -18,6 +18,11 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"GIT,Test1");
+    /** git 指令
+     *   创建并切换到本地分支:git checkout -b VersionXXX
+     *  本地分支push到远程分支(远程无分支): git push origin VersionXXX:VersionXXX
+     *  本地无代码(想要下载远程分支到本地):git clone -b VersionXXX https://github.com/zdrzdr/TestGit.git
+     */
 }
 
 - (void)didReceiveMemoryWarning {
