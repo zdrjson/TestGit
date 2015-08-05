c125bc315b1cdfdeafe74768f3008b1ce4fbab49
张德荣 <zdrjson@sina.com>
2015-08-05 16:55:01 +0800

    本地分支push到远程分支(远程有此分支)的指令

--- TestGit/ViewController.m
+++ TestGit/ViewController.m
@@ -19,9 +19,10 @@
     // Do any additional setup after loading the view, typically from a nib.
     NSLog(@"GIT,Test1");
     /** git 指令
-     *   创建并切换到本地分支:git checkout -b VersionXXX
+     *  创建并切换到本地分支:git checkout -b VersionXXX
      *  本地分支push到远程分支(远程无分支): git push origin VersionXXX:VersionXXX
-     *  本地无代码(想要下载远程分支到本地):git clone -b VersionXXX https://github.com/zdrzdr/TestGit.git
+     *  本地无代码(想要下载远程分支到本地):git clone -b VersionXXX url
+     *  本地分支push到远程分支(远程有此分支):git push origin VersionXXX
      */
 }
 
